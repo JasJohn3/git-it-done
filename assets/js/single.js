edf4fdfe302ca85774601f6be1d4640bcc89783f
Your Name <jasjohn3@uat.edu>
2021-07-22 09:37:58 -0700

    Added Error handling and RepoName functions

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,21 +1,35 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  if(repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  }else {
+    document.location.replace("./index.html");
+  }
+}
+
 var getRepoIssues = function(repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+  // make a get request to url
   fetch(apiUrl).then(function(response) {
     // request was successful
     if (response.ok) {
       response.json().then(function(data) {
         displayIssues(data);
-    
+
         // check if api has paginated issues
         if (response.headers.get("Link")) {
           displayWarning(repo);
         }
       });
-    }
-    else {
-      alert("There was a problem with your request!");
+    } else {
+      // if not successful, redirect to homepage
+      document.location.replace("./index.html");
     }
   });
 };
@@ -63,4 +77,4 @@ var displayWarning = function(repo) {
   // append to warning container
   limitWarningEl.appendChild(linkEl);
 };
-getRepoIssues("facebook/react");
+getRepoName();
